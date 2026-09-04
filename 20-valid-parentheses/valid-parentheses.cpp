4508b9133568bb39136b2b7f64f6f8774f916bbc
shubham sharma <shubamsharma34136@gmail.com>
2026-09-04 14:51:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.26%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                char c = st.top();
+
+                if ((c == '{' && s[i] != '}') ||
+                    (c == '[' && s[i] != ']') ||
+                    (c == '(' && s[i] != ')')) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
