22ab2fe234948d4bc5d2285c780446f5b6a77769
shubham sharma <shubamsharma34136@gmail.com>
2026-09-09 00:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (40.69%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        for(int a:asteroids){
+            while(!st.empty()&&st.top()>0&&a<0){
+                if(st.top()<-a){
+                    st.pop();
+                    
+                }
+                else if(st.top()==-a){
+                    st.pop();
+                    a=0;
+                    break;
+                }
+                else{
+                    a=0;
+                    break;
+                }
+            }
+            if(a!=0){
+                st.push(a);
+            }
+        }
+        vector<int>ans(st.size());
+        for(int i=st.size()-1;i>=0;i--){
+            ans[i]=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
